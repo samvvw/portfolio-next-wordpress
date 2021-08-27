e4d369c35521db0ee673ee7abc3a5b0153bc67ba
Sam Villegas <svillegaswarre00@mylangara.ca>
2021-08-27 12:34:00 -0700

    author section styles

--- components/authorBio/authorBio.tsx
+++ components/authorBio/authorBio.tsx
@@ -1,26 +1,40 @@
 import Image from 'next/image';
+import parse from 'html-react-parser';
+import style from './author.module.scss';
 
 export function AuthorBio({
     authorBios: {
+        bio,
         authorsName,
-        authorTagLine,
+        authorTagline,
         authorPic: { sourceUrl },
     },
+    title,
     excerpt,
 }: WPAPI.AuthorBioProps): JSX.Element {
     return (
-        <section>
-            <h2>{authorsName}</h2>
-            <h3>{authorTagLine}</h3>
-            <div dangerouslySetInnerHTML={{ __html: excerpt }} />
-            <Image
-                src={sourceUrl}
-                width={250}
-                height={250}
-                objectFit={'cover'}
-                objectPosition={'top right'}
-                alt={authorsName}
-            />
+        <section className={style.authorBio}>
+            <div className={style.authorProfile}>
+                <div className={style.taglineWrapper}>
+                    <h3>{authorsName}</h3>
+                    <h4>{authorTagline}</h4>
+                    {parse(excerpt)}
+                </div>
+                <div className={style.entryThumbnail}>
+                    <Image
+                        src={sourceUrl}
+                        width={250}
+                        height={250}
+                        objectFit={'cover'}
+                        objectPosition={'top right'}
+                        alt={authorsName}
+                    />
+                </div>
+            </div>
+            <div className={style.authorBio}>
+                <h2>{title}</h2>
+                <p>{bio}</p>
+            </div>
         </section>
     );
 }
